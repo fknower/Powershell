6d9f8af3944ac0743be08bce36958dc00e851175
Mr Francisco Knower <42910041+fknower@users.noreply.github.com>
2018-09-26 12:01:04 +0100

    Delete Insterller.ps1

--- Insterller.ps1
+++
@@ -1,5 +0,0 @@
-# runs a installer(setup.exe) to automagicaly install applications on a machine
-$InstallerLoc = "LOCATION OF INSTALLER FILE"
-$Switches = "SWITCHES TO PROVIDE TO INSTALLER"
-$Installer = Start-Process -FilePath $InstallerLoc -ArgumentList $Switches -Wait -PassThru
-Write-Host "The exit code is $($Installer.ExitCode)"
