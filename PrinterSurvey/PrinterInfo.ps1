cd29f5c0b72b8f56d2dc115b4f5469e9e2ed3666
Mr Francisco Knower <42910041+fknower@users.noreply.github.com>
2018-10-10 14:43:36 +0100

    Update PrinterInfo.ps1
    Forgot to store results in a file

--- PrinterSurvey/PrinterInfo.ps1
+++ PrinterSurvey/PrinterInfo.ps1
@@ -1,14 +1,10 @@
 #Gets Printer Information on a windows 7 machine. not using get-printer cmdlet
 $a = Get-WmiObject -Class Win32_Printer
-
 #Initilizes Object
 $b = @()
-
 #Removes junk into a new array $_ is the current object
-
 $a | ForEach-Object($_.Name){
 $remove = 0
-
   switch($_.Name){
    "Send To OneNote 2016" {$remove = 1; break}
    "Microsoft XPS Document Writer" {$remove = 1; break}
@@ -18,3 +14,5 @@ $remove = 0
    $b += $_
    }
 }
+#store result in a file
+Set-Content -LiteralPath C:\test\powershell\Printers\PrinterList.txt -Value $b
